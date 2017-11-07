0a6574d3b057844b2dca4d550a19b55feb6d1e71
dylanavery <fossilfuelmusic@gmail.com>
2017-11-07 10:03:31 -0700

    Add schema function to index

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ function __export(m) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./accounting"));
+__export(require("./schema"));
 //# sourceMappingURL=index.js.map
